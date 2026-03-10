d86bbf61062222df9411dfd6d44c2f72a7fdea52
muhammadfadlan31651-collab <muhammad.fadlan31651@sma.belajar.id>
2026-03-10 21:35:21 +0700

    Update soal3.cpp

--- soal3.cpp
+++ soal3.cpp
@@ -4,3 +4,17 @@
     Buatlah program yang menerima input satu bilangan bulat (detik) 
     dan menampilkan output dalam format: X detik = H jam M menit S detik
 */
+
+#include <iostream> // header iostream untuk i/o standar
+using namespace std; // pakai namespace std biar gak ribet tulis std: setiap kali
+int main() { // fungsi utama program
+    int totalDetik, jam, menit, detik;     // Deklarasi variabel:
+    cin >> totalDetik; // baca input bilangan bulat dari user dan disimpan di totaldetik
+    jam = totalDetik / 3600; // Hitung jam: 1 jam sama dengan 3600 detik, pake pembagian bulat
+    menit = (totalDetik % 3600) / 60; // Hitung menit: ambil sisa detik setelah dijam terus bagi 60, pake modulo buat sisa pembagian
+    detik = totalDetik % 60; // Hitung detik akhir: ambil sisa pembagian totalDetik sama 60 buat dapetin detik yang belum cukup jadi menit
+    // Tampilin hasilnya 
+    cout << totalDetik << " detik = " << jam << " jam " << menit << " menit " << detik << " detik" << endl;
+    return 0;
+}
+
